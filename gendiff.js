faf64718d33b5ca9ed59c1c7a579e96c0f21a9cc
Mikhail Makharia <makharia.study@gmail.com>
2024-03-27 13:16:21 +0300

    arguments added

--- gendiff.js
+++ gendiff.js
@@ -5,5 +5,8 @@ program
     .name('gendiff')
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1>')
+    .arguments('<filepath2>')
 program.parse(process.argv);
 const opt = program.opts();
