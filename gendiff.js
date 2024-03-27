a87a645ea5febcfc6e0cfc48bbed7f0f8c51a897
Mikhail Makharia <makharia.study@gmail.com>
2024-03-27 12:20:43 +0300

    gendiff fix

--- gendiff.js
+++ gendiff.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 
-program.command('gendiff')
+program
     .name('gendiff')
+    .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
-    .option('-h, --help', 'output usage information')
-program.parse();
+program.parse(process.argv);
+const opt = program.opts();
