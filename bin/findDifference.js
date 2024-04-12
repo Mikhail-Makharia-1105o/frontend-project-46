624084228db809e4bfeed8b08502643df5110b11
Mikhail Makharia <makharia.study@gmail.com>
2024-04-12 13:12:07 +0300

    small fixes

--- bin/findDifference.js
+++ bin/findDifference.js
@@ -1,16 +1,16 @@
 /**
  * The function finds differences in two object.
  * Returns an array of 3 objects - params only in obj1, params only in obj2 and params in both. Parameters
- * are considered equal if both of their keyues are equal.
+ * are considered equal if both of their keyes are equal.
  * @param {object} obj1
  * @param {object} obj2
- * @returns {Array} Object array of differences
+ * @returns {Array} [ {shared}, {excl1}, {excl2} ]
  */
 export default function findDifference(obj1, obj2) {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
-  const excl2 = {};
   const excl1 = {};
+  const excl2 = {};
   const shared = keys1.reduce((shard, key) => {
     if (keys2.includes(key)) {
       if (obj1[key] === obj2[key]) {
