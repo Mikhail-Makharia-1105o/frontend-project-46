02289470bf86c61d16875e5db080a5069c84bf7a
Mikhail Makharia <makharia.study@gmail.com>
2024-06-10 10:40:58 +0300

    flat fix

--- utils/flatOutputCompare.js
+++ utils/flatOutputCompare.js
@@ -1,6 +1,4 @@
 #!usr/bin/env node
-let output = '';
-
 /**
 * Converts comparison object into a set of flat output strings.
 * @param {Object} comparisonObj - comparison object
@@ -14,38 +12,40 @@ export default function flatOutput(
   originalObj2,
   currentPath = '',
 ) {
+  let output = [];
   const keys = Object.keys(comparisonObj).sort((a, b) => a > b ? 1 : -1);
   keys.forEach((key) => {
     if (comparisonObj[key].nested) {
       if (comparisonObj[key].type === 'added') {
-        flatOutput(
+        output.push(flatOutput(
           comparisonObj[key].value,
           originalObj1,
           originalObj2[key],
           `${currentPath}${currentPath ? '.' : ''}${key}`,
-        );
+        ));
       } else if (comparisonObj[key].type === 'removed') {
-        flatOutput(
+        output.push(flatOutput(
           comparisonObj[key].value,
           originalObj1[key],
           originalObj2,
           `${currentPath}${currentPath ? '.' : ''}${key}`,
-        );
+        ));
       } else if (comparisonObj[key].type === 'changed') {
-        flatOutput(
+        output.push(flatOutput(
           comparisonObj[key].value,
           originalObj1[key],
           originalObj2[key],
           `${currentPath}${currentPath ? '.' : ''}${key}`,
-        );
+        ));
       }
     } else if (comparisonObj[key].type === 'added') {
-      output += `Property '${currentPath}${currentPath ? '.' : ''}${key}' was added with value: ${typeof comparisonObj[key].value === 'object' && comparisonObj[key].value ? '[complex value]' : typeof comparisonObj[key].value === 'string' ? `'${comparisonObj[key].value}'` : comparisonObj[key].value}\n`;
+      output.push(`Property '${currentPath}${currentPath ? '.' : ''}${key}' was added with value: ${typeof comparisonObj[key].value === 'object' && comparisonObj[key].value ? '[complex value]' : typeof comparisonObj[key].value === 'string' ? `'${comparisonObj[key].value}'` : comparisonObj[key].value}`);
     } else if (comparisonObj[key].type === 'removed') {
-      output += `Property '${currentPath}${currentPath ? '.' : ''}${key}' was removed\n`;
+      output.push(`Property '${currentPath}${currentPath ? '.' : ''}${key}' was removed`);
     } else if (comparisonObj[key].type === 'changed') {
-      output += `Property '${currentPath}${currentPath ? '.' : ''}${key}' was updated. From ${typeof originalObj1[key] === 'object' && originalObj1[key] ? '[complex value]' : typeof originalObj1[key] === 'string' ? `'${originalObj1[key]}'` : originalObj1[key]} to ${typeof originalObj2[key] === 'object' && originalObj2[key] ? '[complex value]' : typeof originalObj2[key] === 'string' ? `'${originalObj2[key]}'` : originalObj2[key]}\n`;
+      output.push(`Property '${currentPath}${currentPath ? '.' : ''}${key}' was updated. From ${typeof originalObj1[key] === 'object' && originalObj1[key] ? '[complex value]' : typeof originalObj1[key] === 'string' ? `'${originalObj1[key]}'` : originalObj1[key]} to ${typeof originalObj2[key] === 'object' && originalObj2[key] ? '[complex value]' : typeof originalObj2[key] === 'string' ? `'${originalObj2[key]}'` : originalObj2[key]}`);
     }
   });
-  return output.slice(0, -1);
+  console.log(output);
+  return output.flat(Infinity).join('\n');
 }
